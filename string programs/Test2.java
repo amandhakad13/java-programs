3788a600ae758f28f926ae95ae697f068fe1e564
amandhakad13 <ad5538694@gmail.com>
2025-10-07 20:53:56 -0700

    string is empty or not program

---
+++ string programs/Test2.java
@@ -0,0 +1,12 @@
+public class Test2 {
+	
+	public static void main(String[] args) {
+		
+		String str1 = "Ram Sharma";
+		
+		boolean isPresent = str1.isEmpty();
+		
+		System.out.println(isPresent);
+		
+	}
+}
