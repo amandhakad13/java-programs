e05c2c744b482f93400c48443d53a319bed38330
amandhakad13 <ad5538694@gmail.com>
2025-10-09 08:52:37 -0700

    String equals method and == operator program

---
+++ string programs/Test5.java
@@ -0,0 +1,17 @@
+public class Test5 {
+	
+	public static void main(String[] args) {
+		
+		String str1 = new String("Aman");
+		String str2 = new String("Aman");
+		
+		System.out.println(str1==str2);
+		System.out.println(str1.equals(str2));
+		
+		String str3 = "Deepak";
+		String str4 = "Deepak";
+		
+		System.out.println(str3==str4);
+		System.out.println(str3.equals(str4));
+	}
+}
